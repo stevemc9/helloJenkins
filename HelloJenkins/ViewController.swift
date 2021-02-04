71a5b40d5fdd0d8b0ae67593bebb1bdd8be1a32d
Steve McGuinness <steve.mcguinness@gmail.com>
2021-02-04 10:12:14 -0500

    Initial Commit

--- HelloJenkins/ViewController.swift
+++ HelloJenkins/ViewController.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var myLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Hello Jenkins!"
+        view.textAlignment = NSTextAlignment.center
+        view.textColor = .white
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .systemGreen
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        myLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        myLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        myLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         // Do any additional setup after loading the view.
     }
 
